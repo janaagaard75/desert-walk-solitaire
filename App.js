677f0ad9239ec11c65bde041ed0e9a9a3f60f662
Jan Aagaard <jan@aagaard.net>
2017-07-14 22:14:27 +0200

    Enable TypeScript

--- App.js
+++ App.js
@@ -1,31 +1,3 @@
-// @flow
+import App from './src/App'
 
-import React from 'react'
-import { Component } from 'react'
-import { StyleSheet } from 'react-native'
-import { Text } from 'react-native'
-import { View } from 'react-native'
-
-export default class App extends Component {
-  sum(a: number, b: number) {
-    return a + b
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>2 + 2 = {this.sum(2, 2)}</Text>
-      </View>
-    )
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
+export default App
---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react'
+import { Component } from 'react'
+import { StyleSheet } from 'react-native'
+import { Text } from 'react-native'
+import { View } from 'react-native'
+
+
+
+export default class App extends Component<{}, void> {
+  private styles = StyleSheet.create({
+    container: {
+      alignItems: 'center',
+      backgroundColor: '#fff',
+      flex: 1,
+      justifyContent: 'center',
+    },
+  })
+
+  render() {
+    return (
+      <View style={this.styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>2 + 2 = {this.sum(2, 2)}</Text>
+      </View>
+    )
+  }
+
+  private sum(a: number, b: number) {
+    return a + b
+  }
+}
