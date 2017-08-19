bb8cc7883d97d8db541156f2f003ce42b1f2f509
Jan Aagaard <jan@aagaard.net>
2017-08-19 13:46:03 +0200

    Move variables inside loop

--- src/ArrayUtilities.ts
+++ src/ArrayUtilities.ts
@@ -1,14 +1,11 @@
 export class ArrayUtilities {
   /** Shuffle the elements in an array in place. Based on http://stackoverflow.com/a/962890/37147. */
   public static shuffleArray<T>(array: Array<T>): void {
-    let temporaryElement: T
-    let current: number
     let top = array.length
-
     if (top >= 0) {
       while (--top) {
-        current = Math.floor(Math.random() * (top + 1))
-        temporaryElement = array[current]
+        const current = Math.floor(Math.random() * (top + 1))
+        const temporaryElement = array[current]
         array[current] = array[top]
         array[top] = temporaryElement
       }
