8beaa36825c285abcd753a6359d19cc05be0f478
Jan Aagaard <jan@aagaard.net>
2017-08-20 16:44:00 +0200

    Only shuffle cards when stuck

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -148,9 +148,11 @@ export class Footer extends Component<Props, State> {
     })
   }
 
-  // TODO: Only allow clicking the button if the game is stuck. Don't allow clicking on it, if the game is done.
   private shuffleCardsInWrongPlace() {
-    this.props.shuffleCardsInWrongPlace()
+    // TODO: Should disable the button instead.
+    if (this.props.gameStatus === GameStatus.ShuffleCards) {
+      this.props.shuffleCardsInWrongPlace()
+    }
   }
 
   private startOver() {
