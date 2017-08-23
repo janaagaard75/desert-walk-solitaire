4e2cf0605da818a1b7781c67a687c800d59c6d61
Jan Aagaard <jan@aagaard.net>
2017-08-23 20:42:05 +0200

    Sizes calculated based on card size

--- src/CardView.tsx
+++ src/CardView.tsx
@@ -27,10 +27,10 @@ export class CardView extends Component<Props, {}> {
       borderColor: this.props.isInCorrectPlace ? '#4a4' : this.props.isDraggable ? 'black' : '#b88',
       borderRadius: Math.floor(this.props.size.width / 6),
       borderStyle: 'solid',
-      borderWidth: Math.floor(this.props.size.width / 20),
+      borderWidth: Math.floor(this.props.size.width / 41 * 3),
       height: this.props.size.height,
       overflow: 'hidden',
-      padding: Math.floor(this.props.size.width / 20),
+      padding: Math.floor(this.props.size.width / 41 * 2),
       width: this.props.size.width
     }
 
@@ -38,33 +38,33 @@ export class CardView extends Component<Props, {}> {
       Object.assign(cardStyle, {
         shadowColor: 'black',
         shadowOffset: {
-          height: 2,
-          width: 1
+          height: Math.floor(this.props.size.width / 41 * 2),
+          width: Math.floor(this.props.size.width / 41 * 1)
         },
         shadowOpacity: 0.3,
-        shadowRadius: 3
+        shadowRadius: Math.floor(this.props.size.width / 41 * 3)
       })
     }
 
     const valueStyle: TextStyle = {
       color: Suit.isBlack(this.props.card.suit) ? 'black' : 'red',
-      fontSize: 38,
+      fontSize: Math.floor(this.props.size.width / 41 * 38),
       fontWeight: '700',
-      left: -5,
-      letterSpacing: -3,
+      left: -Math.floor(this.props.size.width / 41 * 5),
+      letterSpacing: -Math.floor(this.props.size.width / 41 * 3),
       position: 'absolute',
-      top: -7,
-      width: 50 // Make space for the two digits in '10'.
+      top: -Math.floor(this.props.size.width / 41 * 7),
+      width: Math.floor(this.props.size.width / 41 * 50) // Make space for the two digits in '10'.
     }
 
     const suitStyle: TextStyle = {
       backgroundColor: 'transparent',
-      bottom: -5,
+      bottom: -Math.floor(this.props.size.width / 41 * 5),
       color: Suit.isBlack(this.props.card.suit) ? 'black' : 'red',
-      fontSize: 40,
+      fontSize: Math.floor(this.props.size.width / 41 * 40),
       fontWeight: '900',
       position: 'absolute',
-      right: -5
+      right: -Math.floor(this.props.size.width / 41 * 5)
     }
 
     return (
