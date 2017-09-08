cdc7e3f9203b08baadf8f962eb6010c8cf766099
Jan Aagaard <jan@aagaard.net>
2017-09-08 20:09:13 +0200

    Simplify code with @computed

--- src/Card.ts
+++ src/Card.ts
@@ -11,15 +11,25 @@ export class Card {
     public readonly suit: Suit,
     public readonly value: number,
     private readonly settings: Settings
-  ) {
-    this.displayValue = this.getDisplayValue()
-  }
+  ) { }
 
-  public readonly displayValue: string
   public next: Card | undefined
   @observable public position: Point
 
-  private getDisplayValue(): string {
+  @computed
+  public get boundary(): Rectangle {
+    const surface = new Rectangle(
+      this.position.x,
+      this.position.x + this.settings.cardSize.width,
+      this.position.y,
+      this.position.y + this.settings.cardSize.height
+    )
+
+    return surface
+  }
+
+  @computed
+  public get displayValue(): string {
     switch (this.value) {
       case 1:
         return 'A'
@@ -36,17 +46,5 @@ export class Card {
       default:
         return this.value.toString()
     }
-  }
-
-  @computed
-  public get boundary(): Rectangle {
-    const surface = new Rectangle(
-      this.position.x,
-      this.position.x + this.settings.cardSize.width,
-      this.position.y,
-      this.position.y + this.settings.cardSize.height
-    )
-
-    return surface
   }
 }
