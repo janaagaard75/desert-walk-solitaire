cd17322022e6066197f28f0d43055a6f7e065094
Jan Aagaard <jan@aagaard.net>
2017-09-29 20:10:17 +0200

    Add TODO

--- src/DraggedCardBoundary.ts
+++ src/DraggedCardBoundary.ts
@@ -1,6 +1,7 @@
 import { Card } from './Card'
 import { Rectangle } from './Rectangle'
 
+// TODO: Rename to BoundaryCardPair.
 export interface DraggedCardBoundary {
   boundary: Rectangle
   card: Card
