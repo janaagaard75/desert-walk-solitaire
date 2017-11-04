4a1debddcb0a76804129a91daed189fb3242633e
Jan Aagaard <jan@aagaard.net>
2017-11-04 22:36:32 +0100

    Add toString

--- src/Suit.ts
+++ src/Suit.ts
@@ -4,3 +4,15 @@ export enum Suit {
   Hearts,
   Spades,
 }
+
+// tslint:disable-next-line:no-namespace
+export namespace Suit {
+  export const toString = (suit: Suit): string => {
+    switch (suit) {
+      case Suit.Clubs: return '♣'
+      case Suit.Diamonds: return '♦'
+      case Suit.Hearts: return '♥'
+      case Suit.Spades: return '♠'
+    }
+  }
+}
