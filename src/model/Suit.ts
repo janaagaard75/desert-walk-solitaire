82d24446582cc1e76307cdcbddc76bf9b7964b5c
Jan Aagaard <jan@aagaard.net>
2018-11-26 22:59:49 +0100

    Upgrade to Expo 32

--- src/model/Suit.ts
+++ src/model/Suit.ts
@@ -1,36 +1,6 @@
-import { Settings } from './Settings'
-
 export enum Suit {
   Clubs,
   Diamonds,
   Hearts,
   Spades
-}
-
-// tslint:disable-next-line:no-namespace
-export namespace Suit {
-  export const character = (suit: Suit): string => {
-    switch (suit) {
-      case Suit.Clubs: return '♣'
-      case Suit.Diamonds: return '♦'
-      case Suit.Hearts: return '♥'
-      case Suit.Spades: return '♠'
-    }
-  }
-
-  export const color = (suit: Suit): string => {
-    switch (suit) {
-      case Suit.Clubs:
-        return Settings.instance.colors.card.clubs
-
-      case Suit.Diamonds:
-        return Settings.instance.colors.card.diamonds
-
-      case Suit.Hearts:
-        return Settings.instance.colors.card.hearts
-
-      case Suit.Spades:
-        return Settings.instance.colors.card.spades
-    }
-  }
 }
