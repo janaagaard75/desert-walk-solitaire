67eab2320e223528db6381c03c495938ce796ffb
Jan Aagaard <jan@aagaard.net>
2019-08-28 22:48:42 +0200

    Upgrade packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,7 +19,13 @@ module.exports = {
     }
   },
   rules: {
-    "@typescript-eslint/array-type": ["error", "generic"],
+    "@typescript-eslint/array-type": [
+      "error",
+      {
+        default: "generic",
+        readonly: "generic"
+      }
+    ],
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-use-before-define": "off",
