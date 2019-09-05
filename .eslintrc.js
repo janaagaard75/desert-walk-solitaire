2bde4de7cb6a68f85298670524a9c633f88451b4
Jan Aagaard <jaa@cvation.com>
2019-09-05 08:50:14 +0200

    Allow parameter properties

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,6 +28,7 @@ module.exports = {
     ],
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-parameter-properties": "off",
     "@typescript-eslint/no-use-before-define": "off",
     "@typescript-eslint/no-unused-vars": "off"
   }
