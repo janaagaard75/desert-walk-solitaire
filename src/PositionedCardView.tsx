6910856cb909619f6753397a36e4303bda9c89bd
Jan Aagaard <jan@aagaard.net>
2019-10-04 22:30:10 +0200

    Add removed code (f)

--- src/PositionedCardView.tsx
+++ src/PositionedCardView.tsx
@@ -100,6 +100,38 @@ export class PositionedCardView extends Component<Props> {
   private panResponder: PanResponderInstance
   @observable private visualState: VisualState = VisualState.Idle
 
+  // public UNSAFE_componentWillReceiveProps(nextProps: Props) {
+  //   if (!this.animate) {
+  //     return
+  //   }
+
+  //   const animateFromOffset = this.props.positionedCard.position.subtract(
+  //     nextProps.positionedCard.position
+  //   )
+  //   this.animatedPosition.setValue(animateFromOffset)
+  //   this.visualState = VisualState.Animating
+
+  //   Animated.timing(this.animatedPosition, {
+  //     duration: Settings.instance.animation.turn.duration,
+  //     easing: Easing.elastic(Settings.instance.animation.turn.elasticity),
+  //     toValue: { x: 0, y: 0 }
+  //   }).start(() => {
+  //     if (this.visualState !== VisualState.Dragging) {
+  //       this.visualState = VisualState.Idle
+  //     }
+  //   })
+  // }
+
+  // private animate(nextProps: Props): boolean {
+  //   return (
+  //     Game.instance.animateNextTurn ||
+  //     Game.instance.animateFromPreviousPosition ||
+  //     !this.props.positionedCard.position.equals(
+  //       nextProps.positionedCard.position
+  //     )
+  //   )
+  // }
+
   public render() {
     this.animatedPosition.setOffset(this.props.positionedCard.position)
 
