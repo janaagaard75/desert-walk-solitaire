87599210247c91b9027f656eb9cea4f713d463a1
Jan Aagaard <jan@aagaard.net>
2019-10-20 22:51:28 +0200

    Remove 'computed'

--- src/model/EmptyCell.ts
+++ src/model/EmptyCell.ts
@@ -1,4 +1,3 @@
-import { computed } from "mobx"
 import { Card } from "./Card"
 import { Cell } from "./Cell"
 import { Deck } from "./Deck"
@@ -8,10 +7,13 @@ import { GridState } from "./GridState"
 export class EmptyCell extends GridCell {
   public constructor(cell: Cell, gridState: GridState) {
     super(cell, gridState)
+
+    this.droppableCards = this.getDroppableCards()
   }
 
-  @computed
-  public get droppableCards(): ReadonlyArray<Card> {
+  public droppableCards: ReadonlyArray<Card>
+
+  private getDroppableCards(): ReadonlyArray<Card> {
     if (this.left === undefined) {
       return Deck.instance.theFourAces
     }
