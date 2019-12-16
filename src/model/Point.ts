339d0ce35af640c4be46cc341aebb2f820277707
Jan Aagaard <jan@aagaard.net>
2019-12-16 21:08:06 +0100

    Remove unused method

--- src/model/Point.ts
+++ src/model/Point.ts
@@ -3,11 +3,6 @@ import { PointInterface } from "./PointInterface"
 export class Point implements PointInterface {
   public constructor(public x: number, public y: number) {}
 
-  public add(other: PointInterface): Point {
-    const sum = new Point(this.x + other.x, this.y + other.y)
-    return sum
-  }
-
   public equals(other: Point): boolean {
     const areEqual = this.x === other.x && this.y === other.y
     return areEqual
