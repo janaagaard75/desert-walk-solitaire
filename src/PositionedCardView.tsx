fb0fa6b708e57b40d37236775626dbe48372caf2
Jan Aagaard <janaagaard75@users.noreply.github.com>
2019-12-25 20:57:38 +0100

    6 pixels threshold

--- src/PositionedCardView.tsx
+++ src/PositionedCardView.tsx
@@ -86,7 +86,7 @@ export class PositionedCardView extends Component<Props> {
   private animatedPosition: Animated.ValueXY
   private panResponder: PanResponderInstance
   // TODO: Bigger threshold, perhaps mainly on bigger screens?
-  private readonly moveThreshold = 4
+  private readonly moveThreshold = 6
   @observable private visualState: VisualState
 
   public componentDidUpdate(prevProps: Props, _prevState: State) {
