4421affd7ea07319827f2e78f72bd3967b022e81
Jan Aagaard <jan@aagaard.net>
2020-05-12 22:01:07 +0200

    Explain rule customizations with comments

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,38 @@
-{
-  "parser": "@typescript-eslint/parser",
-  "extends": [
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: [
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
     "prettier/@typescript-eslint",
     "plugin:prettier/recommended"
   ],
-  "parserOptions": {
-    "project": "./tsconfig.json",
-    "sourceType": "module"
+  parserOptions: {
+    project: "./tsconfig.json",
+    sourceType: "module"
   },
-  "settings": {
-    "react": {
-      "version": "detect"
+  settings: {
+    react: {
+      version: "detect"
     }
   },
-  "env": { "browser": true, "node": true },
-  "rules": {
+  env: { browser: true, node: true },
+  rules: {
     "@typescript-eslint/array-type": [
       "error",
       {
-        "default": "generic",
-        "readonly": "generic"
+        default: "generic",
+        readonly: "generic"
       }
     ],
+    // Mandatory return types is too verbose for my taste.
     "@typescript-eslint/explicit-function-return-type": "off",
+    // Empty constructors make sense for singleton classes.
     "@typescript-eslint/no-empty-function": [
       "error",
-      { "allow": ["constructors"] }
+      { allow: ["constructors"] }
     ],
+    // Let TypeScript test for unused variables.
     "@typescript-eslint/no-unused-vars": "off"
   }
 }
