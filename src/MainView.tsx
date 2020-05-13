09372cb98566e67f62a13a736ecbb6287708b95f
Jan Aagaard <jaa@cvation.com>
2020-05-13 11:03:10 +0200

    Add trailing commas

--- src/MainView.tsx
+++ src/MainView.tsx
@@ -11,7 +11,7 @@ import {
   Text,
   TouchableWithoutFeedback,
   View,
-  ViewStyle
+  ViewStyle,
 } from "react-native"
 import appJson from "../app.json"
 import "./ArrayExtensions"
@@ -40,13 +40,13 @@ export class MainView extends Component {
     backgroundColor: Settings.colors.mainBackgroundColor,
     flex: 1,
     flexDirection: "column",
-    position: "relative"
+    position: "relative",
   }
 
   private gridWrapperStyle: ViewStyle = {
     alignItems: "center",
     flex: 1,
-    justifyContent: "center"
+    justifyContent: "center",
   }
 
   public render() {
@@ -75,7 +75,7 @@ export class MainView extends Component {
                 height: ComputedSettings.instance.windowSize.height,
                 position: "absolute",
                 resizeMode: "repeat",
-                width: ComputedSettings.instance.windowSize.width
+                width: ComputedSettings.instance.windowSize.width,
               }}
             />
             <GridView />
@@ -85,12 +85,10 @@ export class MainView extends Component {
         {ComputedSettings.isIosWithoutHomeButton() ? (
           <View
             style={{
-              height: 15
+              height: 15,
             }}
           />
-        ) : (
-          undefined
-        )}
+        ) : undefined}
         <Text
           style={{
             color: "#fff",
@@ -98,7 +96,7 @@ export class MainView extends Component {
             fontSize: 9,
             right: 30,
             position: "absolute",
-            top: 2
+            top: 2,
           }}
         >
           Version: {this.versionNumber}
@@ -113,7 +111,7 @@ export class MainView extends Component {
 
   private async loadFont() {
     await Font.loadAsync({
-      "Arabian-onenightstand": require("../assets/xxii-arabian-onenightstand/xxii-arabian-onenightstand.ttf")
+      "Arabian-onenightstand": require("../assets/xxii-arabian-onenightstand/xxii-arabian-onenightstand.ttf"),
     })
   }
 
@@ -123,7 +121,7 @@ export class MainView extends Component {
     const windowSize = Dimensions.get("window")
     ComputedSettings.instance.windowSize = {
       height: windowSize.height,
-      width: windowSize.width
+      width: windowSize.width,
     }
   }
 }
