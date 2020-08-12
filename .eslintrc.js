a4fc68383ab78b9c889137cb037d0860c067e496
Jan Aagaard <jaa@cvation.com>
2020-08-12 08:47:38 +0200

    Remove parserOptions for now

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,13 +4,17 @@ module.exports = {
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
+    // Consider adding these rules. This requires parserOptions.project to be set, and ESLint will then complain about .js files not being included.
+    // "plugin:@typescript-eslint/recommended-requiring-type-checking",
     "prettier/@typescript-eslint",
     "plugin:prettier/recommended",
   ],
-  parserOptions: {
-    project: "./tsconfig.json",
-    sourceType: "module",
-  },
+  // parserOptions: {
+  //   project: "./tsconfig.json",
+  //   sourceType: "module",
+  // },
+  // Ignore .js files since we're coding in TypeScript.
+  // "ignorePatterns": ["**/*.js"],
   settings: {
     react: {
       version: "detect",
