6054549437dcfe5dcd400b145f51b0942653e6c0
Jan Aagaard <jan@aagaard.net>
2020-11-10 22:22:30 +0100

    Upgrade to Mobx 6, without makeObservable

--- App.ts
+++ App.ts
@@ -1,2 +1,6 @@
+import { configure } from "mobx"
 import { MainView } from "./src/MainView"
+
+// See https://github.com/mobxjs/mobx/blob/main/docs/migrating-from-4-or-5.md#getting-started.
+configure({ enforceActions: "never" })
 export default MainView
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,10 @@
 module.exports = function (api) {
   api.cache(true)
   return {
+    plugins: [
+      ["@babel/plugin-proposal-decorators", { legacy: true }],
+      ["@babel/plugin-proposal-class-properties", { loose: false }],
+    ],
     presets: ["babel-preset-expo"],
   }
 }
