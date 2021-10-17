42037b5e1cba0a83abc1c055b039ad36e71dc5d8
Jan Aagaard <jan@aagaard.net>
2021-10-17 20:47:30 +0200

    Use functional style in TouchableIcon

--- src/TouchableIcon.tsx
+++ src/TouchableIcon.tsx
@@ -1,5 +1,5 @@
 import { Entypo, FontAwesome, MaterialCommunityIcons } from "@expo/vector-icons"
-import React, { Component } from "react"
+import React from "react"
 import { TouchableOpacity, View } from "react-native"
 import { TouchableState } from "./model/TouchableState"
 
@@ -16,61 +16,50 @@ interface Props {
   state: TouchableState
 }
 
-export class TouchableIcon extends Component<Props> {
-  public render() {
-    const numberOfIcons = 7
-    const width = (1 / numberOfIcons) * 100
+const numberOfIcons = 7
+const width = (1 / numberOfIcons) * 100
+const iconSize = 20
 
-    if (this.props.state === TouchableState.Hidden) {
-      return <View style={{ width: `${width}%` }} />
-    }
-
-    const color = this.props.state === TouchableState.Enabled ? "#fff" : "#999"
-    const shadowOpacity = this.props.state === TouchableState.Enabled ? 0.5 : 0
-
-    return (
-      <TouchableOpacity
-        onPress={this.handlePress}
-        disabled={this.props.state === TouchableState.Disabled}
-        style={{
-          alignItems: "center",
-          alignSelf: "center",
-          backgroundColor: "transparent",
-          shadowColor: "#fff",
-          shadowOpacity: shadowOpacity,
-          shadowRadius: 5,
-          width: `${width}%`,
-        }}
-      >
-        {this.renderIcon(this.props.iconName, color)}
-      </TouchableOpacity>
-    )
+export const TouchableIcon = (props: Props) => {
+  if (props.state === TouchableState.Hidden) {
+    return <View style={{ width: `${width}%` }} />
   }
 
-  private handlePress = () => {
-    this.props.handlePress()
-  }
+  const color = props.state === TouchableState.Enabled ? "#fff" : "#999"
+  const shadowOpacity = props.state === TouchableState.Enabled ? 0.5 : 0
 
-  private renderIcon(iconName: IconName, color: string): JSX.Element {
-    const iconSize = 20
+  return (
+    <TouchableOpacity
+      onPress={props.handlePress}
+      disabled={props.state === TouchableState.Disabled}
+      style={{
+        alignItems: "center",
+        alignSelf: "center",
+        backgroundColor: "transparent",
+        shadowColor: "#fff",
+        shadowOpacity: shadowOpacity,
+        shadowRadius: 5,
+        width: `${width}%`,
+      }}
+    >
+      {renderIcon(props.iconName, color)}
+    </TouchableOpacity>
+  )
+}
 
-    switch (iconName) {
-      case "controller-fast-forward":
-      case "shuffle":
-        return <Entypo color={color} name={iconName} size={iconSize} />
+const renderIcon = (iconName: IconName, color: string): JSX.Element => {
+  switch (iconName) {
+    case "controller-fast-forward":
+    case "shuffle":
+      return <Entypo color={color} name={iconName} size={iconSize} />
 
-      case "fast-backward":
-        return <FontAwesome color={color} name={iconName} size={iconSize} />
+    case "fast-backward":
+      return <FontAwesome color={color} name={iconName} size={iconSize} />
 
-      case "redo":
-      case "undo":
-        return (
-          <MaterialCommunityIcons
-            color={color}
-            name={iconName}
-            size={iconSize}
-          />
-        )
-    }
+    case "redo":
+    case "undo":
+      return (
+        <MaterialCommunityIcons color={color} name={iconName} size={iconSize} />
+      )
   }
 }
