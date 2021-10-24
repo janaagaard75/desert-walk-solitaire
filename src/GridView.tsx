03c831881ef1df984fee1c80099459da97c62ed3
Jan Aagaard <jan@aagaard.net>
2021-10-24 23:15:40 +0200

    Use functional style in GridView

--- src/GridView.tsx
+++ src/GridView.tsx
@@ -1,35 +1,30 @@
 import { observer } from "mobx-react"
-import React, { Component } from "react"
+import React from "react"
 import { View, ViewStyle } from "react-native"
 import { EmptyCellView } from "./EmptyCellView"
 import { ComputedSettings } from "./model/ComputedSettings"
 import { Game } from "./model/Game"
 import { PositionedCardView } from "./PositionedCardView"
 
-@observer
-export class GridView extends Component {
-  public render() {
-    const gridViewStyle: ViewStyle = {
-      height: ComputedSettings.instance.gridSize.height,
-      position: "relative",
-      width: ComputedSettings.instance.gridSize.width,
-    }
-
-    return (
-      <View style={gridViewStyle}>
-        {Game.instance.currentGridState.positionedCards.map(
-          (positionedCard) => (
-            <PositionedCardView
-              key={positionedCard.card.key}
-              cardSize={ComputedSettings.instance.cardSize}
-              positionedCard={positionedCard}
-            />
-          )
-        )}
-        {Game.instance.currentGridState.emptyCells.map((emptyCell) => (
-          <EmptyCellView emptyCell={emptyCell} key={emptyCell.cell.key} />
-        ))}
-      </View>
-    )
+export const GridView = observer(() => {
+  const gridViewStyle: ViewStyle = {
+    height: ComputedSettings.instance.gridSize.height,
+    position: "relative",
+    width: ComputedSettings.instance.gridSize.width,
   }
-}
+
+  return (
+    <View style={gridViewStyle}>
+      {Game.instance.currentGridState.positionedCards.map((positionedCard) => (
+        <PositionedCardView
+          key={positionedCard.card.key}
+          cardSize={ComputedSettings.instance.cardSize}
+          positionedCard={positionedCard}
+        />
+      ))}
+      {Game.instance.currentGridState.emptyCells.map((emptyCell) => (
+        <EmptyCellView emptyCell={emptyCell} key={emptyCell.cell.key} />
+      ))}
+    </View>
+  )
+})
