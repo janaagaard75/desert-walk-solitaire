07da9bc8364c9475939ccb63f9fcbf2742e618ee
Jan Aagaard <jan@aagaard.net>
2024-07-23 21:57:04 +0200

    Convert to function components

--- src/MainView.tsx
+++ src/MainView.tsx
@@ -1,16 +1,14 @@
 import { loadAsync } from "expo-font";
 import { lockAsync, OrientationLock } from "expo-screen-orientation";
 import * as SplashScreen from "expo-splash-screen";
-import { autorun, makeObservable, observable } from "mobx";
-import { observer } from "mobx-react";
-import { PureComponent } from "react";
+import { observer } from "mobx-react-lite";
+import React, { useEffect, useState } from "react";
 import {
   Dimensions,
   Image,
   Text,
   TouchableWithoutFeedback,
   View,
-  ViewStyle,
 } from "react-native";
 import appJson from "../app.json";
 import "./ArrayExtensions";
@@ -23,115 +21,103 @@ import { Settings } from "./model/Settings";
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 SplashScreen.preventAutoHideAsync();
 
-interface Props {}
+export const MainView = observer(() => {
+  const [fontLoaded, setFontLoaded] = useState(false);
+  const [versionNumberVisible, setVersionNumberVisible] = useState(false);
 
-@observer
-export class MainView extends PureComponent<Props> {
-  public constructor(props: Props) {
-    super(props);
+  useEffect(() => {
+    lockAsync(OrientationLock.LANDSCAPE);
 
-    makeObservable<MainView, "fontLoaded" | "showVersionNumber">(this);
-
-    void lockAsync(OrientationLock.LANDSCAPE);
-
-    this.updateWindowSize();
+    updateWindowSize();
     Dimensions.addEventListener("change", () => {
-      this.updateWindowSize();
+      updateWindowSize();
     });
 
-    // eslint-disable-next-line @typescript-eslint/no-floating-promises
-    this.loadFont();
+    loadFont();
+  }, []);
 
-    autorun(async () => {
-      if (this.fontLoaded) {
-        await SplashScreen.hideAsync();
-      }
-    });
-  }
-
-  @observable private fontLoaded = false;
-  @observable private showVersionNumber = false;
-
-  private mainViewStyle: ViewStyle = {
-    backgroundColor: Settings.colors.mainBackgroundColor,
-    flex: 1,
-    flexDirection: "column",
-    position: "relative",
-  };
-
-  private gridWrapperStyle: ViewStyle = {
-    alignItems: "center",
-    flex: 1,
-    justifyContent: "center",
-  };
-
-  public render() {
-    if (!this.fontLoaded) {
-      return null;
-    }
-
-    return (
-      <View style={this.mainViewStyle}>
-        <TouchableWithoutFeedback
-          delayLongPress={5 * 1000}
-          onLongPress={() => (this.showVersionNumber = true)}
-        >
-          <View style={this.gridWrapperStyle}>
-            <Image
-              source={require("../assets/50713-transparent.png")}
-              style={{
-                backgroundColor: Settings.colors.gridBackgroundColor,
-                height: ComputedSettings.instance.windowSize.height,
-                position: "absolute",
-                resizeMode: "repeat",
-                width: ComputedSettings.instance.windowSize.width,
-              }}
-            />
-            <GridView />
-          </View>
-        </TouchableWithoutFeedback>
-        <FooterView />
-        {ComputedSettings.isIosWithoutHomeButton() ? (
-          <View
-            style={{
-              height: 15,
-            }}
-          />
-        ) : undefined}
-        <Text
-          style={{
-            color: "#fff",
-            display: this.showVersionNumber ? "flex" : "none",
-            fontSize: 9,
-            right: 30,
-            position: "absolute",
-            top: 2,
-          }}
-        >
-          Version: {this.versionNumber}
-        </Text>
-      </View>
-    );
-  }
-
-  private get versionNumber(): string {
-    return appJson.expo.version;
-  }
-
-  private async loadFont() {
+  const loadFont = async () => {
     await loadAsync({
       "Arabian-onenightstand": require("../assets/xxii-arabian-onenightstand/xxii-arabian-onenightstand.ttf"),
     });
-    this.fontLoaded = true;
+
+    setFontLoaded(true);
+
+    await SplashScreen.hideAsync();
+  };
+
+  const showVersionNumber = () => {
+    setVersionNumberVisible(true);
+  };
+
+  if (!fontLoaded) {
+    return <></>;
   }
+
+  return (
+    <View
+      style={{
+        backgroundColor: Settings.colors.mainBackgroundColor,
+        flex: 1,
+        flexDirection: "column",
+        position: "relative",
+      }}
+    >
+      <TouchableWithoutFeedback
+        delayLongPress={5 * 1000}
+        onLongPress={showVersionNumber}
+      >
+        <View
+          style={{
+            alignItems: "center",
+            flex: 1,
+            justifyContent: "center",
+          }}
+        >
+          <Image
+            source={require("../assets/50713-transparent.png")}
+            style={{
+              backgroundColor: Settings.colors.gridBackgroundColor,
+              height: ComputedSettings.instance.windowSize.height,
+              position: "absolute",
+              resizeMode: "repeat",
+              width: ComputedSettings.instance.windowSize.width,
+            }}
+          />
+          <GridView />
+        </View>
+      </TouchableWithoutFeedback>
+      <FooterView />
+      {ComputedSettings.isIosWithoutHomeButton() ? (
+        <View
+          style={{
+            height: 15,
+          }}
+        />
+      ) : undefined}
+      <Text
+        style={{
+          color: "#fff",
+          display: versionNumberVisible ? "flex" : "none",
+          fontSize: 9,
+          right: 30,
+          position: "absolute",
+          top: 2,
+        }}
+      >
+        Version: {appJson.expo.version}
+      </Text>
+    </View>
+  );
 
   // TODO: When replaying a finished game, the animation pauses a bit after replaying a shuffle turn.
   // TODO: Fix resizing when rotating the iPad.
-  private updateWindowSize() {
-    const windowSize = Dimensions.get("window");
-    ComputedSettings.instance.windowSize = {
-      height: windowSize.height,
-      width: windowSize.width,
-    };
-  }
-}
+});
+
+const updateWindowSize = () => {
+  const windowSize = Dimensions.get("window");
+  ComputedSettings.instance.windowSize = {
+    height: windowSize.height,
+    width: windowSize.width,
+  };
+};
--- src/PositionedCardView.tsx
+++ src/PositionedCardView.tsx
@@ -1,12 +1,6 @@
-import { computed, makeObservable, observable } from "mobx";
-import { observer } from "mobx-react";
-import { PureComponent } from "react";
-import {
-  Animated,
-  Easing,
-  PanResponder,
-  PanResponderInstance,
-} from "react-native";
+import { observer } from "mobx-react-lite";
+import React, { useEffect, useMemo, useState } from "react";
+import { Animated, Easing, PanResponder } from "react-native";
 import { CardView } from "./CardView";
 import { ComputedSettings } from "./model/ComputedSettings";
 import { Game } from "./model/Game";
@@ -20,129 +14,104 @@ interface Props {
   positionedCard: PositionedCard;
 }
 
-interface State {
-  visualState: VisualState;
-}
+export const PositionedCardView = observer((props: Props) => {
+  const [visualState, setVisualState] = useState(VisualState.Idle);
 
-@observer
-export class PositionedCardView extends PureComponent<Props> {
-  public constructor(props: Props) {
-    super(props);
+  const draggable = Game.instance.currentGridState.draggableCards.includes(
+    props.positionedCard.card
+  );
 
-    makeObservable<PositionedCardView, "visualState" | "draggable">(this);
+  const animatedPosition = useMemo(
+    () => new Animated.ValueXY(props.positionedCard.position),
+    [props.positionedCard.position]
+  );
 
-    this.visualState = VisualState.Idle;
+  // TODO: Bigger threshold, perhaps mainly on bigger screens?
+  const moveThreshold = 8;
+  const panResponder = useMemo(
+    () =>
+      PanResponder.create({
+        onStartShouldSetPanResponder: (_e, _gestureState) => true,
+        onPanResponderGrant: (_e, _gestureState) => {
+          Game.instance.cardDragStarted(props.positionedCard);
+          setVisualState(VisualState.Dragging);
+        },
+        onPanResponderMove: (_e, gestureState) => {
+          animatedPosition.setValue({
+            x: props.positionedCard.position.x + gestureState.dx,
+            y: props.positionedCard.position.y + gestureState.dy,
+          });
+        },
+        onPanResponderEnd: (_e, gestureState) => {
+          console.log("onPanResponderEnd");
+          // TODO: The isPress logic does not take into account that the card might have been dragged away and the back to the original position, when letting go. If that has happened, this is not a 'press'.
+          const isPress =
+            Math.abs(gestureState.dx) <= moveThreshold &&
+            Math.abs(gestureState.dy) <= moveThreshold;
 
-    this.animatedPosition = new Animated.ValueXY(
-      this.props.positionedCard.position
-    );
+          if (isPress) {
+            // Moves the card to the first available target. This is only called on cards that are draggable. Returns the vector used for the animating the move.
+            Game.instance.moveCardToFirstTarget(props.positionedCard);
+            return;
+          }
 
-    this.animatedPosition.addListener((position) => {
+          if (Game.instance.cardDropped()) {
+            return;
+          }
+
+          Animated.timing(animatedPosition, {
+            duration: Settings.animation.snap.duration,
+            easing: Easing.elastic(Settings.animation.snap.elasticity),
+            toValue: {
+              x: props.positionedCard.position.x,
+              y: props.positionedCard.position.y,
+            },
+            useNativeDriver: true,
+          }).start(() => {
+            setVisualState(VisualState.Idle);
+          });
+        },
+      }),
+    [animatedPosition, props.positionedCard]
+  );
+
+  useEffect(() => {
+    animatedPosition.addListener((position) => {
       const boundary = ComputedSettings.instance.getCardBoundary(position);
       Game.instance.cardDragged(boundary);
     });
+  }, [animatedPosition]);
 
-    this.panResponder = PanResponder.create({
-      onStartShouldSetPanResponder: (_e, _gestureState) => true,
-      onPanResponderGrant: (_e, _gestureState) => {
-        Game.instance.cardDragStarted(this.props.positionedCard);
-        this.visualState = VisualState.Dragging;
-      },
-      onPanResponderMove: (_e, gestureState) => {
-        this.animatedPosition.setValue({
-          x: this.props.positionedCard.position.x + gestureState.dx,
-          y: this.props.positionedCard.position.y + gestureState.dy,
-        });
-      },
-      onPanResponderEnd: (_e, gestureState) => {
-        // TODO: The isPress logic does not take into account that the card might have been dragged away and the back to the original position, when letting go. If that has happened, this is not a 'press'.
-        const isPress =
-          Math.abs(gestureState.dx) <= this.moveThreshold &&
-          Math.abs(gestureState.dy) <= this.moveThreshold;
+  useEffect(() => {
+    setVisualState(VisualState.Animating);
 
-        if (isPress) {
-          this.moveToTarget();
-          return;
-        }
-
-        if (Game.instance.cardDropped()) {
-          return;
-        }
-
-        Animated.timing(this.animatedPosition, {
-          duration: Settings.animation.snap.duration,
-          easing: Easing.elastic(Settings.animation.snap.elasticity),
-          toValue: {
-            x: this.props.positionedCard.position.x,
-            y: this.props.positionedCard.position.y,
-          },
-          useNativeDriver: true,
-        }).start(() => {
-          this.visualState = VisualState.Idle;
-        });
-      },
-    });
-  }
-
-  private animatedPosition: Animated.ValueXY;
-  private panResponder: PanResponderInstance;
-  // TODO: Bigger threshold, perhaps mainly on bigger screens?
-  private readonly moveThreshold = 8;
-  @observable private visualState: VisualState;
-
-  public componentDidUpdate(prevProps: Props, _prevState: State) {
-    if (
-      this.props.positionedCard.position.equals(
-        prevProps.positionedCard.position
-      )
-    ) {
-      return;
-    }
-
-    this.visualState = VisualState.Animating;
-
-    Animated.spring(this.animatedPosition, {
-      toValue: this.props.positionedCard.position,
+    Animated.spring(animatedPosition, {
+      toValue: props.positionedCard.position,
       useNativeDriver: true,
     }).start(() => {
-      this.visualState = VisualState.Idle;
+      setVisualState(VisualState.Idle);
     });
-  }
+  }, [animatedPosition, props.positionedCard.position]);
 
-  public render() {
-    return (
-      <Animated.View
-        style={{
-          position: "absolute",
-          transform: [
-            { translateX: this.animatedPosition.x },
-            { translateY: this.animatedPosition.y },
-          ],
-          zIndex: this.visualState === VisualState.Idle ? 1 : 2,
-        }}
-        {...(this.draggable ? this.panResponder.panHandlers : undefined)}
-      >
-        <CardView
-          card={this.props.positionedCard.card}
-          cardSize={this.props.cardSize}
-          correctlyPlaced={this.props.positionedCard.correctlyPlaced}
-          draggable={this.draggable}
-          dragging={this.visualState === VisualState.Dragging}
-        />
-      </Animated.View>
-    );
-  }
-
-  @computed
-  private get draggable(): boolean {
-    const draggable = Game.instance.currentGridState.draggableCards.includes(
-      this.props.positionedCard.card
-    );
-    return draggable;
-  }
-
-  /** Moves the card to the first available target. This is only called on cards that are draggable. Returns the vector used for the animating the move. */
-  private moveToTarget(): void {
-    Game.instance.moveCardToFirstTarget(this.props.positionedCard);
-  }
-}
+  return (
+    <Animated.View
+      style={{
+        position: "absolute",
+        transform: [
+          { translateX: animatedPosition.x },
+          { translateY: animatedPosition.y },
+        ],
+        zIndex: visualState === VisualState.Idle ? 1 : 2,
+      }}
+      {...(draggable ? panResponder.panHandlers : undefined)}
+    >
+      <CardView
+        card={props.positionedCard.card}
+        cardSize={props.cardSize}
+        correctlyPlaced={props.positionedCard.correctlyPlaced}
+        draggable={draggable}
+        dragging={visualState === VisualState.Dragging}
+      />
+    </Animated.View>
+  );
+});
